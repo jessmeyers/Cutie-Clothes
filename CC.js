73d9c089ad34b618aac157af8ee43d859d2d8e01
jessmeyers <53497726+jessmeyers@users.noreply.github.com>
2019-08-02 15:00:36 -0400

    Add files via upload

---
+++ CC.js
@@ -0,0 +1,32 @@
+<center><img src="girl2.jpg" width="270" height="400" alt="homepic">
+Long-Sleeve Floral Full-Length Dress: $22.99</center>
+<br>
+<br>
+<center><img src="girl4.jpg" width="300" height="350" alt="homepic">
+Blue and White Striped Dress Embroidered with Bees and Flowers: $26.99</center>
+<br>
+<br>
+<center><img src="girl5.jpg" width="300" height=350" alt="homepic">
+Cheeta-Print Tshirt Dress:
+$17.00</center>
+<br>
+<br>
+<center><img src="girl6.jpg" width="350" height="350" alt="homepic">
+White Satin Dress with Red and yellow Flowers:
+$32.00</center>
+<br>
+<br>
+<center><img src="girl8.jpg" width="250" height="350" alt="homepic">
+Flowy Tropical Print Dress with Thin Straps:
+$24.99</center>
+<br>
+<br>
+<center><img src="image1.jpg" width="300" height="350" alt="homepic">
+Pink Tool Dress with White Lace Detail and Pink Flower Attatchment:
+$39.99</center>
+<br>
+<br>
+<center>  <img src="image111.jpg" width="250" height="350" alt="homepic">
+White Tool Dress with Dark Pink Ribbon and Sparkles:
+$36.00</center>
+<br>
